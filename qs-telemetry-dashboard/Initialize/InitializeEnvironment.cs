1f54d5a758c18dc21887c4b1f121c586a4ac0a1f
Jesse Paris <jparis@gmail.com>
2020-09-23 21:40:48 -0700

    cleaning up more errors

--- qs-telemetry-dashboard/Initialize/InitializeEnvironment.cs
+++ qs-telemetry-dashboard/Initialize/InitializeEnvironment.cs
@@ -4,6 +4,7 @@ using qs_telemetry_dashboard.Models;
 using qs_telemetry_dashboard.QRSHelpers;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -15,9 +16,18 @@ namespace qs_telemetry_dashboard.Initialize
 {
 	internal class InitializeEnvironment
 	{
-		internal static int Run(Logger logger, QRSRequest qrsRequest)
+		private readonly Logger _logger;
+		private readonly QRSRequest _qrsRequest;
+
+		internal InitializeEnvironment(Logger logger, QRSRequest qrsRequest)
 		{
-			logger.Log("Running in initialize mode.", LogLevel.Info);
+			_logger = logger;
+			_qrsRequest = qrsRequest;
+		}
+		internal int Run()
+		{
+			//todo this is sloppy. should be encapsulated or nonstatic
+			_logger.Log("Running in initialize mode.", LogLevel.Info);
 
 
 
@@ -37,7 +47,7 @@ namespace qs_telemetry_dashboard.Initialize
 
 			
 
-			public static ActionResult ValidateInstallDir(Session session)
+			public string ValidateInstallDir()
 			{
 				string installDir = session["INSTALLFOLDER"];
 				session.Log("Install directory to validate: " + installDir);
@@ -75,18 +85,18 @@ namespace qs_telemetry_dashboard.Initialize
 				catch (ArgumentException e)
 				{
 					session.Message(InstallMessage.Error, new Record() { FormatString = "The install directory was not valid:\n" + e.Message });
-					return ActionResult.Failure;
+					return "Failure";
 				}
 				catch (Exception e)
 				{
 					session.Message(InstallMessage.Error, new Record() { FormatString = "The install directory validation failed:\n" + e.Message });
-					return ActionResult.Failure;
+					return "Failure";
 				}
 
-				return ActionResult.Success;
+				return "Success";
 			}
 
-			public static ActionResult SetOutputDir(Session session)
+			public string SetOutputDir()
 			{
 				string installDir = session.CustomActionData["InstallDir"];
 				string outputDir = Path.Combine(installDir, METADATA_OUTPUT);
@@ -100,16 +110,16 @@ namespace qs_telemetry_dashboard.Initialize
 				text = text.Replace("outputFolderPlaceholder", outputDir);
 				File.WriteAllText(installDir + JS_LIBRARY_FOLDER + "\\config\\config.js", text);
 
-				return ActionResult.Success;
+				return "Success";
 			}
 
-			public static ActionResult IsRepositoryRunning(Session session)
+			public string IsRepositoryRunning()
 			{
 				session.Log("Making a request to 'https://{0}:4242", HOSTNAME.Value);
-				Tuple<HttpStatusCode, string> response = MakeQrsRequest("/about", HttpMethod.Get);
+				Tuple<HttpStatusCode, string> response = _qrsRequest.MakeRequest("/about", HttpMethod.Get);
 				if (response.Item1 == HttpStatusCode.OK)
 				{
-					return ActionResult.Success;
+					return "Success";
 				}
 				else
 				{
@@ -117,16 +127,16 @@ namespace qs_telemetry_dashboard.Initialize
 					session.Log("Response was: {0}", response.Item1.ToString());
 
 					session.Message(InstallMessage.Error, new Record() { FormatString = "Cannot install the Telemetry Dashboard as the installer could not contact the 'Qlik Repository Service'. Response code was: " + response.Item1.ToString() });
-					return ActionResult.Failure;
+					return "Failure";
 				}
 			}
 
-			public static ActionResult ImportApp(Session session)
+			public string ImportApp()
 			{
-				Tuple<HttpStatusCode, string> apps = MakeQrsRequest("/app/full?filter=name eq 'Telemetry Dashboard'", HttpMethod.Get);
+				Tuple<HttpStatusCode, string> apps = _qrsRequest.MakeRequest("/app/full?filter=name eq 'Telemetry Dashboard'", HttpMethod.Get);
 				if (apps.Item1 != HttpStatusCode.OK)
 				{
-					return ActionResult.Failure;
+					return "Failure";
 				}
 
 				JArray listOfApps = JArray.Parse(apps.Item2);
@@ -134,10 +144,10 @@ namespace qs_telemetry_dashboard.Initialize
 				if (listOfApps.Count == 1)
 				{
 					string appID = listOfApps[0]["id"].ToString();
-					Tuple<HttpStatusCode, string> replaceAppResponse = MakeQrsRequest("/app/upload/replace?targetappid=" + appID, HttpMethod.Post, HTTPContentType.app, Properties.Resources.Telemetry_Dashboard);
+					Tuple<HttpStatusCode, string> replaceAppResponse = _qrsRequest.MakeRequest("/app/upload/replace?targetappid=" + appID, HttpMethod.Post, HTTPContentType.app, Properties.Resources.Telemetry_Dashboard);
 					if (replaceAppResponse.Item1 == HttpStatusCode.Created)
 					{
-						return ActionResult.Success;
+						return "Success";
 					}
 				}
 
@@ -150,24 +160,24 @@ namespace qs_telemetry_dashboard.Initialize
 							listOfApps[i]["name"] = listOfApps[i]["name"] + "-old";
 							listOfApps[i]["modifiedDate"] = DateTime.UtcNow.ToString("s") + "Z";
 							string appId = listOfApps[i]["id"].ToString();
-							Tuple<HttpStatusCode, string> updatedApp = MakeQrsRequest("/app/" + appId, HttpMethod.Put, HTTPContentType.json, Encoding.UTF8.GetBytes(listOfApps[i].ToString()));
+							Tuple<HttpStatusCode, string> updatedApp = _qrsRequest.MakeRequest("/app/" + appId, HttpMethod.Put, HTTPContentType.json, Encoding.UTF8.GetBytes(listOfApps[i].ToString()));
 							if (updatedApp.Item1 != HttpStatusCode.OK)
 							{
-								return ActionResult.Failure;
+								return "Failure";
 							}
 						}
 					}
 
-					Tuple<HttpStatusCode, string> uploadAppResponse = MakeQrsRequest("/app/upload?name=Telemetry Dashboard", HttpMethod.Post, HTTPContentType.app, Properties.Resources.Telemetry_Dashboard);
+					Tuple<HttpStatusCode, string> uploadAppResponse = _qrsRequest.MakeRequest("/app/upload?name=Telemetry Dashboard", HttpMethod.Post, HTTPContentType.app, Properties.Resources.Telemetry_Dashboard);
 					if (uploadAppResponse.Item1 == HttpStatusCode.Created)
 					{
-						return ActionResult.Success;
+						return "Success";
 					}
 				}
-				return ActionResult.Failure;
+				return "Failure";
 			}
 
-			public static ActionResult CreateTasks(Session session)
+			public string CreateTasks()
 			{
 				string installDir = session.CustomActionData["InstallDir"];
 				if (!installDir.EndsWith("\\"))
@@ -177,10 +187,10 @@ namespace qs_telemetry_dashboard.Initialize
 
 				string externalTaskID = "";
 				// External Task
-				Tuple<HttpStatusCode, string> hasExternalTask = MakeQrsRequest("/externalprogramtask/count?filter=name eq 'TelemetryDashboard-1-Generate-Metadata'", HttpMethod.Get);
+				Tuple<HttpStatusCode, string> hasExternalTask = _qrsRequest.MakeRequest("/externalprogramtask/count?filter=name eq 'TelemetryDashboard-1-Generate-Metadata'", HttpMethod.Get);
 				if (hasExternalTask.Item1 != HttpStatusCode.OK)
 				{
-					return ActionResult.Failure;
+					return "Failure";
 				}
 				if (JObject.Parse(hasExternalTask.Item2)["value"].ToObject<int>() == 0)
 				{
@@ -197,10 +207,10 @@ namespace qs_telemetry_dashboard.Initialize
 					'impactSecurityAccess': false,
 					'schemaPath': 'ExternalProgramTask'
 				}";
-					Tuple<HttpStatusCode, string> createExternalTask = MakeQrsRequest("/externalprogramtask", HttpMethod.Post, HTTPContentType.json, Encoding.UTF8.GetBytes(body));
+					Tuple<HttpStatusCode, string> createExternalTask = _qrsRequest.MakeRequest("/externalprogramtask", HttpMethod.Post, HTTPContentType.json, Encoding.UTF8.GetBytes(body));
 					if (createExternalTask.Item1 != HttpStatusCode.Created)
 					{
-						return ActionResult.Failure;
+						return "Failure";
 					}
 					else
 					{
@@ -209,22 +219,22 @@ namespace qs_telemetry_dashboard.Initialize
 				}
 				else
 				{
-					Tuple<HttpStatusCode, string> getExternalTaskId = MakeQrsRequest("/externalprogramtask?filter=name eq 'TelemetryDashboard-1-Generate-Metadata'", HttpMethod.Get);
+					Tuple<HttpStatusCode, string> getExternalTaskId = _qrsRequest.MakeRequest("/externalprogramtask?filter=name eq 'TelemetryDashboard-1-Generate-Metadata'", HttpMethod.Get);
 					externalTaskID = JArray.Parse(getExternalTaskId.Item2)[0]["id"].ToString();
 
 				}
 
 				// Reload Task
-				Tuple<HttpStatusCode, string> reloadTasks = MakeQrsRequest("/reloadtask/full?filter=name eq 'TelemetryDashboard-2-Reload-Dashboard'", HttpMethod.Get);
+				Tuple<HttpStatusCode, string> reloadTasks = _qrsRequest.MakeRequest("/reloadtask/full?filter=name eq 'TelemetryDashboard-2-Reload-Dashboard'", HttpMethod.Get);
 				if (reloadTasks.Item1 != HttpStatusCode.OK)
 				{
-					return ActionResult.Failure;
+					return "Failure";
 				}
 
 				JArray listOfTasks = JArray.Parse(reloadTasks.Item2);
 
 				// Get AppID for Telemetry Dashboard App
-				Tuple<HttpStatusCode, string> getAppID = MakeQrsRequest("/app?filter=name eq 'Telemetry Dashboard'", HttpMethod.Get);
+				Tuple<HttpStatusCode, string> getAppID = _qrsRequest.MakeRequest("/app?filter=name eq 'Telemetry Dashboard'", HttpMethod.Get);
 				string appId = JArray.Parse(getAppID.Item2)[0]["id"].ToString();
 
 				if (listOfTasks.Count == 0)
@@ -276,10 +286,10 @@ namespace qs_telemetry_dashboard.Initialize
 						}
 					}";
 
-					Tuple<HttpStatusCode, string> importExtensionResponse = MakeQrsRequest("/reloadtask/create", HttpMethod.Post, HTTPContentType.json, Encoding.UTF8.GetBytes(body));
+					Tuple<HttpStatusCode, string> importExtensionResponse = _qrsRequest.MakeRequest("/reloadtask/create", HttpMethod.Post, HTTPContentType.json, Encoding.UTF8.GetBytes(body));
 					if (importExtensionResponse.Item1 != HttpStatusCode.Created)
 					{
-						return ActionResult.Failure;
+						return "Failure";
 					}
 				}
 				else
@@ -287,26 +297,26 @@ namespace qs_telemetry_dashboard.Initialize
 					listOfTasks[0]["app"] = JObject.Parse(@"{ 'id': '" + appId + "'}");
 					listOfTasks[0]["modifiedDate"] = DateTime.UtcNow.ToString("s") + "Z";
 					string reloadTaskID = listOfTasks[0]["id"].ToString();
-					Tuple<HttpStatusCode, string> updatedApp = MakeQrsRequest("/reloadtask/" + reloadTaskID, HttpMethod.Put, HTTPContentType.json, Encoding.UTF8.GetBytes(listOfTasks[0].ToString()));
+					Tuple<HttpStatusCode, string> updatedApp = _qrsRequest.MakeRequest("/reloadtask/" + reloadTaskID, HttpMethod.Put, HTTPContentType.json, Encoding.UTF8.GetBytes(listOfTasks[0].ToString()));
 					if (updatedApp.Item1 != HttpStatusCode.OK)
 					{
-						return ActionResult.Failure;
+						return "Failure";
 					}
 				}
 
-				return ActionResult.Success;
+				return "Success";
 			}
 
-			public static ActionResult CreateDataConnections(Session session)
+			public string CreateDataConnections()
 			{
 				string installDir = session.CustomActionData["InstallDir"];
 				installDir = installDir.Replace("\\", "\\\\");
 
 				// Add TelemetryMetadata dataconnection
-				Tuple<HttpStatusCode, string> dataConnections = MakeQrsRequest("/dataconnection?filter=name eq 'TelemetryMetadata'", HttpMethod.Get);
+				Tuple<HttpStatusCode, string> dataConnections = _qrsRequest.MakeRequest("/dataconnection?filter=name eq 'TelemetryMetadata'", HttpMethod.Get);
 				if (dataConnections.Item1 != HttpStatusCode.OK)
 				{
-					return ActionResult.Failure;
+					return "Failure";
 				}
 				JArray listOfDataconnections = JArray.Parse(dataConnections.Item2);
 				if (listOfDataconnections.Count == 0)
@@ -320,10 +330,10 @@ namespace qs_telemetry_dashboard.Initialize
 				}";
 
 
-					Tuple<HttpStatusCode, string> createdConnection = MakeQrsRequest("/dataconnection", HttpMethod.Post, HTTPContentType.json, Encoding.UTF8.GetBytes(body));
+					Tuple<HttpStatusCode, string> createdConnection = MakeRequest("/dataconnection", HttpMethod.Post, HTTPContentType.json, Encoding.UTF8.GetBytes(body));
 					if (createdConnection.Item1 != HttpStatusCode.Created)
 					{
-						return ActionResult.Failure;
+						return "Failure";
 					}
 				}
 				else
@@ -332,18 +342,18 @@ namespace qs_telemetry_dashboard.Initialize
 					listOfDataconnections[0]["connectionstring"] = installDir + METADATA_OUTPUT + "\\";
 					listOfDataconnections[0]["modifiedDate"] = DateTime.UtcNow.ToString("s") + "Z";
 					string appId = listOfDataconnections[0]["id"].ToString();
-					Tuple<HttpStatusCode, string> updatedConnection = MakeQrsRequest("/dataconnection/" + appId, HttpMethod.Put, HTTPContentType.json, Encoding.UTF8.GetBytes(listOfDataconnections[0].ToString()));
+					Tuple<HttpStatusCode, string> updatedConnection = _qrsRequest.MakeRequest("/dataconnection/" + appId, HttpMethod.Put, HTTPContentType.json, Encoding.UTF8.GetBytes(listOfDataconnections[0].ToString()));
 					if (updatedConnection.Item1 != HttpStatusCode.OK)
 					{
-						return ActionResult.Failure;
+						return "Failure";
 					}
 				}
 
 				// Add EngineSettings dataconnection
-				Tuple<HttpStatusCode, string> engineSettingDataconnection = MakeQrsRequest("/dataconnection?filter=name eq 'EngineSettingsFolder'", HttpMethod.Get);
+				Tuple<HttpStatusCode, string> engineSettingDataconnection = _qrsRequest.MakeRequest("/dataconnection?filter=name eq 'EngineSettingsFolder'", HttpMethod.Get);
 				if (dataConnections.Item1 != HttpStatusCode.OK)
 				{
-					return ActionResult.Failure;
+					return "Failure";
 				}
 				listOfDataconnections = JArray.Parse(engineSettingDataconnection.Item2);
 				if (listOfDataconnections.Count == 0)
@@ -356,52 +366,52 @@ namespace qs_telemetry_dashboard.Initialize
 					'username': ''
 				}";
 
-					Tuple<HttpStatusCode, string> createdConnection = MakeQrsRequest("/dataconnection", HttpMethod.Post, HTTPContentType.json, Encoding.UTF8.GetBytes(body));
+					Tuple<HttpStatusCode, string> createdConnection = _qrsRequest.MakeRequest("/dataconnection", HttpMethod.Post, HTTPContentType.json, Encoding.UTF8.GetBytes(body));
 					if (createdConnection.Item1 != HttpStatusCode.Created)
 					{
-						return ActionResult.Failure;
+						return "Failure";
 					}
 				}
 
-				return ActionResult.Success;
+				return "Success";
 			}
 
-			public static ActionResult CopyCertificates(Session session)
+			public string CopyCertificates()
 			{
 				string installDir = session.CustomActionData["InstallDir"];
 				File.Copy(@"C:\ProgramData\Qlik\Sense\Repository\Exported Certificates\.Local Certificates\root.pem", Path.Combine(installDir, JS_LIBRARY_FOLDER, "certs\\root.pem"), true);
 				File.Copy(@"C:\ProgramData\Qlik\Sense\Repository\Exported Certificates\.Local Certificates\client.pem", Path.Combine(installDir, JS_LIBRARY_FOLDER, "certs\\client.pem"), true);
 				File.Copy(@"C:\ProgramData\Qlik\Sense\Repository\Exported Certificates\.Local Certificates\client_key.pem", Path.Combine(installDir, JS_LIBRARY_FOLDER, "certs\\client_key.pem"), true);
 
-				return ActionResult.Success;
+				return "Success";
 			}
 
-			public static ActionResult RemoveTasks(Session session)
+			public string RemoveTasks()
 			{
-				Tuple<HttpStatusCode, string> getReloadTaskId = MakeQrsRequest("/reloadtask?filter=name eq 'TelemetryDashboard-2-Reload-Dashboard'", HttpMethod.Get);
+				Tuple<HttpStatusCode, string> getReloadTaskId = _qrsRequest.MakeRequest("/reloadtask?filter=name eq 'TelemetryDashboard-2-Reload-Dashboard'", HttpMethod.Get);
 				if (getReloadTaskId.Item1 == HttpStatusCode.OK)
 				{
 					JArray reloadTasks = JArray.Parse(getReloadTaskId.Item2);
 					foreach (JToken t in reloadTasks)
 					{
-						MakeQrsRequest("/reloadtask/" + t["id"], HttpMethod.Delete);
+						_qrsRequest.MakeRequest("/reloadtask/" + t["id"], HttpMethod.Delete);
 					}
 				}
 
-				Tuple<HttpStatusCode, string> getExternalTaskId = MakeQrsRequest("/externalprogramtask?filter=name eq 'TelemetryDashboard-1-Generate-Metadata'", HttpMethod.Get);
+				Tuple<HttpStatusCode, string> getExternalTaskId = _qrsRequest.MakeRequest("/externalprogramtask?filter=name eq 'TelemetryDashboard-1-Generate-Metadata'", HttpMethod.Get);
 				if (getExternalTaskId.Item1 == HttpStatusCode.OK)
 				{
 					JArray externalTasks = JArray.Parse(getExternalTaskId.Item2);
 					foreach (JToken t in externalTasks)
 					{
-						MakeQrsRequest("/externalprogramtask/" + t["id"], HttpMethod.Delete);
+						_qrsRequest.MakeRequest("/externalprogramtask/" + t["id"], HttpMethod.Delete);
 					}
 				}
 
-				return ActionResult.Success;
+				return "Success";
 			}
 
-			private static X509Certificate2 SetTLSandGetCertificate()
+			private X509Certificate2 SetTLSandGetCertificate()
 			{
 				ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 				var clientPem = File.ReadAllText(@"C:\ProgramData\Qlik\Sense\Repository\Exported Certificates\.Local Certificates\client.pem");
@@ -416,7 +426,7 @@ namespace qs_telemetry_dashboard.Initialize
 				return cert;
 			}
 
-			private static string GetHostnameFromConfig()
+			private string GetHostnameFromConfig()
 			{
 				string hostnameBase64 = File.ReadAllText(@"C:\ProgramData\Qlik\Sense\Host.cfg");
 				byte[] data = Convert.FromBase64String(hostnameBase64);
